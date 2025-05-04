14a624dca2bb7ba9a7e900f630567df9bcd23fe6
Noah Villarreal <noah.villarreal1@gmail.com>
2025-05-03 20:30:17 -0700

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
@@ -47,3 +48,30 @@ export default function App() {
     
   );
 }
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 9f40d55 (Initialize project using Create React App)
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 // src/App.test.js
 test('App loads without crashing', () => {
   expect(true).toBe(true);
+=======
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+>>>>>>> 9f40d55 (Initialize project using Create React App)
 });
